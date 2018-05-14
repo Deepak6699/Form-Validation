cb2ac05862c3df2a95c34db847ab814b951e1027
Deepak Chandragiri <deepak.rockstar1231@gmail.com>
2018-05-14 22:50:11 +0530

    final commit

--- Form Validation/app.js
+++ Form Validation/app.js
@@ -13,11 +13,29 @@ function validateName() {
     }
 }
 function validateZipCode(){
-
+    const zip = document.getElementById('zip');
+    const re = /^[0-9]{5}(-[0-9]{4})?$/;
+    if (!re.test(zip.value)) {
+        zip.classList.add('is-invalid');
+    } else {
+        zip.classList.remove('is-invalid');
+    }
 }
 function validateEmail(){
-
+    const email = document.getElementById('email');
+    const re = /^([A-Za-z0-9_\-\.]+)\@([A-Za-z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    if (!re.test(email.value)) {
+        email.classList.add('is-invalid');
+    } else {
+        email.classList.remove('is-invalid');
+    }
 }
 function validatePhone(){
-
+    const phone = document.getElementById('phone');
+    const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/
+    if (!re.test(phone.value)) {
+        phone.classList.add('is-invalid');
+    } else {
+        phone.classList.remove('is-invalid');
+    }
 }
